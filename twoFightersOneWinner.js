ba896e6abef0fa51ec2685c908c9df24253d373e
thomas <thomaalanstevens@gmail.com>
2022-04-11 22:16:18 -0700

    Completed twoFightersOneWinner challenge.

---
+++ twoFightersOneWinner.js
@@ -0,0 +1,25 @@
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  
+    while(fighter1.health > 0 && fighter2.health > 0 && firstAttacker == fighter1.name){
+      fighter2.health -= fighter1.damagePerAttack
+      if(fighter2.health <= 0){
+        return fighter1.name 
+      }
+      fighter1.health -= fighter2.damagePerAttack
+      if(fighter1.health <= 0){
+        return fighter2.name
+      }
+    } 
+    
+    while(fighter1.health > 0 && fighter2.health > 0 && firstAttacker == fighter2.name){
+      fighter1.health -= fighter2.damagePerAttack
+      if(fighter1.health <= 0){
+        return fighter2.name 
+      }
+      fighter2.health -= fighter1.damagePerAttack
+      if(fighter2.health <= 0){
+        return fighter1.name
+      }
+    } 
+  
+  }
