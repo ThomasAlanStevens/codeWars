6ce868ec91f7d7a2cbd318676b9a5ea305b6a5f4
thomas <thomaalanstevens@gmail.com>
2022-05-07 22:31:01 -0700

    Completed doesMyNumberLookBigInThis challenge.

---
+++ doesMyNumberLookBigInThis.js
@@ -0,0 +1,5 @@
+function narcissistic(value) {
+    console.log(String(value).split('').reduce((total, num, i, arr) => total += num**arr.length))
+    return String(value).split('').reduce((total, num, i, arr) => total += num**arr.length, 0) == value ? true : false
+  }
+  
