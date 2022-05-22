ddc582f9283966b54ac9963279451c6e21108a92
thomas <thomaalanstevens@gmail.com>
2022-05-22 00:38:07 -0700

    fixed formatting.

--- complementaryDna.js
+++ complementaryDna.js
@@ -1,6 +1,6 @@
 function DNAStrand(dna){
     let dnaArray = dna.split('')
-    for(let i = 0; i < dna.length; i++){
+    for(let i = 0; i < dnaArray.length; i++){
         dnaArray[i] == 'A' ? dnaArray[i] = 'T' :
         dnaArray[i] == 'T' ? dnaArray[i] = 'A' :
         dnaArray[i] == 'C' ? dnaArray[i] = 'G' :
