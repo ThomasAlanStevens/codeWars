c21d125c35374d259526101d7f58bed5d5f21ebf
thomas <thomaalanstevens@gmail.com>
2022-08-25 06:52:46 -0700

    Completed moving values sql challenge.

---
+++ movingValues.sql
@@ -0,0 +1,3 @@
+SELECT
+  LENGTH(CAST(name as text)) as id, LENGTH(CAST(legs as text)) as name, LENGTH(CAST(arms as text)) as legs, LENGTH(CAST(characteristics as text)) as arms, LENGTH(CAST(id as text)) as characteristics
+FROM monsters
